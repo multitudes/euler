70898ab80766bd0366ff823dbc4f4e7ca4e0e4d7
laurent <wrmultitudes@gmail.com>
2020-05-31 17:47:31 +0200

    versioning

---
+++ Sources/euler/Assets/Descriptions/descriptionForChallenge20.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Laurent B on 31/05/2020.
+//
+
+import Foundation
+
+public let descriptionForChallenge19 = """
+\nProject Euler: Problem 20: Factorial digit sum\n
+\n
+
+
+n! means n × (n − 1) × ... × 3 × 2 × 1\n
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,\n
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.\n
+
+Find the sum of the digits n! \n\n
+
+https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-20-factorial-digit-sum\n
+"""
---
+++ Sources/euler/Assets/Inputs/inputsForChallenge20.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Laurent B on 31/05/2020.
+//
+
+import Foundation
+
+public let (firstYear, lastYear) = (1901,2001)
+
+//public let (firstYear, lastYear) = (1995, 2000)
+//public let (firstYear, lastYear) = (1943, 1946)
