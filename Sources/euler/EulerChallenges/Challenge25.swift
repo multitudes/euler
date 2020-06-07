7fad81ec5af94daa5507c84c6cb7ebcc81fe0a93
laurent <wrmultitudes@gmail.com>
2020-06-07 12:59:19 +0200

    challenge 25

--- Sources/euler/EulerChallenges/Challenge25.swift
+++ Sources/euler/EulerChallenges/Challenge25.swift
@@ -17,15 +17,17 @@ public class Challenge25 {
     }
    
     func digitFibonacci(for digits: Int) -> Int {
-        // the fibonacci sequence mathematically is a asymptotic function
-        if digits >= 20 { 
+        // the fibonacci sequence mathematically is a asymptotic function - The formula below (see the screen shot in images)
+        // is tested and works well. tested from 5 digits
+        if digits >= 5 {
             let phi = 1.61803399 // golden ratio
             print(log(10.0)) //. in swift log(10) is the natural logarithm
             print(pow(2.71828, 2.302585092994046))
-            // here digits - 1 is because again 1000 has 4 digits so is 10^3 
+            // here digits - 1 is because again 1000 has 4 digits so is 10^3
             let number = (log(10.0) * Double(digits - 1) + log(5.0) / 2 ) / log(phi)
             return Int(ceil(number))
             }
+        // this was the brute force method. Works till 19 digits
         // Ex if the digits are three I divide by 100 to check if more than three digits
         // 100 is 10 ^ 2
         let digitCheck: Int = Int(pow(10.0, Double(digits-1)))
